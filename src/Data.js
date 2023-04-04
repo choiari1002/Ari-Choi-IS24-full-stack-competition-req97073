9dedcd6ea577697f39df41cf099ce51a597b7dde
Ari Choi <choiari1002@gmail.com>
2023-04-04 01:18:07 -0700

    Create dummy data for products

---
+++ src/Data.js
@@ -0,0 +1,79 @@
+let data = [
+  {
+    productId: 1,
+    productName: "Project A",
+    productOwnerName: "John Smith",
+    Developers: [
+      "Mary Brown",
+      "David Lee",
+      "Lisa Chen",
+      "Jason Kim",
+      "Amanda Johnson"
+    ],
+    scrumMasterName: "Michael Davis",
+    startDate: "2022/01/01",
+    methodology: "Scrum"
+  },
+  {
+    productId: 2,
+    productName: "Project B",
+    productOwnerName: "Sarah Johnson",
+    Developers: [
+      "John Smith",
+      "David Lee",
+      "Jason Kim",
+      "Amanda Johnson",
+      "Julia Lee"
+    ],
+    scrumMasterName: "Jennifer Lee",
+    startDate: "2021/11/01",
+    methodology: "Kanban"
+  },
+  {
+    productId: 3,
+    productName: "Project C",
+    productOwnerName: "Mark Taylor",
+    Developers: [
+      "David Lee",
+      "Lisa Chen",
+      "Jason Kim",
+      "Julia Lee",
+      "Michael Davis"
+    ],
+    scrumMasterName: "Sarah Johnson",
+    startDate: "2022/03/01",
+    methodology: "Scrum"
+  },
+  {
+    productId: 4,
+    productName: "Project D",
+    productOwnerName: "Lisa Chen",
+    Developers: [
+      "John Smith",
+      "David Lee",
+      "Amanda Johnson",
+      "Julia Lee",
+      "Michael Davis"
+    ],
+    scrumMasterName: "Mark Taylor",
+    startDate: "2021/12/01",
+    methodology: "Kanban"
+  },
+  {
+    productId: 5,
+    productName: "Project E",
+    productOwnerName: "Jason Kim",
+    Developers: [
+      "David Lee",
+      "Lisa Chen",
+      "Amanda Johnson",
+      "Julia Lee",
+      "Jennifer Lee"
+    ],
+    scrumMasterName: "John Smith",
+    startDate: "2022/02/01",
+    methodology: "Scrum"
+  }
+]
+
+export default data;
