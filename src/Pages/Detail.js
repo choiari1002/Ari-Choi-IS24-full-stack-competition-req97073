0f7c949474b039a51fb31ea3d13a5edd92e1518b
Ari Choi <choiari1002@gmail.com>
2023-04-17 18:15:10 -0700

    fix bug

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -2,34 +2,34 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
-
-    useEffect(()=> {
-    })
-
     let {id} = useParams();
     let product = props.products.find(function(product){
-        return product.productId == id
-      });
+      return product.productId == id
+    });
 
-    return(
-        <div className="container">
-            <div className="row">
-                <h4>{ product.productName } (ID : {product.productId})</h4>
-                <p>Product Owner Name : { product.productOwnerName }</p>
-                <p>Developers :</p>
-                {
-                    product.Developers.map(function(developer,i){
-                        return(
-                            <p>- { developer }</p>
-                        )
-                    })
-                }
-                <p>Scrum Master Name : { product.scrumMasterName }</p>
-                <p>Start Date : { product.startDate }</p>
-                <p>Methodology : { product.methodology }</p>
-            </div>
+    if (!product) {
+      return <div>Product not found.</div>;
+    }
+
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{product.productName} (ID : {product.productId})</h4>
+          <p>Product Owner Name : {product.productOwnerName}</p>
+          <p>Developers :</p>
+          {
+            product.Developers.map(function(developer,i){
+              return(
+                <p key={i}>- {developer}</p>
+              )
+            })
+          }
+          <p>Scrum Master Name : {product.scrumMasterName}</p>
+          <p>Start Date : {product.startDate}</p>
+          <p>Methodology : {product.methodology}</p>
         </div>
-    )
+      </div>
+    );
   }
 
   export default Detail;
